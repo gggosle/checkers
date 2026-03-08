47daccdf9f37d1c0941cd39e210a3d1bf65048d7
gggosle <gakhovalera2004@gmail.com>
2026-03-08 12:31:23 +0200

    Refactor piece initialization to use Checker class and simplify board setup

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const COLS_NUM = 8;
 const PIECE_ROWS_COUNT = 3;
 const BLACK_PIECE = 2;
 const WHITE_PIECE = 1;
-const EMPTY_CELL = 0;
 const CELL_CLASS = 'cell';
 const BLACK_CELL_CLASS = 'black';
 const WHITE_CELL_CLASS = 'white';
@@ -14,28 +13,38 @@ const HIGHLIGHT_CLASS = 'highlight';
 const boardElement = document.getElementById('board');
 const boardData = initializeBoardData();
 
+class Checker {
+    constructor(color, row, col, direction) {
+        this.color = color;
+        this.row = row;
+        this.col = col;
+        this.direction = direction;
+        this.isKing = false;
+    }
+}
 
 function initializeBoardData() {
     const data = [];
     for (let row = 0; row < ROWS_NUM; row++) {
         const rowArray = [];
         for (let col = 0; col < COLS_NUM; col++) {
+            let pieceToCreate = null;
+
             if (isBlackSquare(row, col)) {
                 if (row < PIECE_ROWS_COUNT) {
-                    rowArray.push(WHITE_PIECE);
+                    pieceToCreate = new Checker(WHITE_PIECE, row, col, 1);
                 } else if (row >= ROWS_NUM - PIECE_ROWS_COUNT) {
-                    rowArray.push(BLACK_PIECE);
-                } else {
-                    rowArray.push(EMPTY_CELL);
+                    pieceToCreate = new Checker(BLACK_PIECE, row, col, -1);
                 }
-            } else {
-                rowArray.push(EMPTY_CELL);
             }
+
+            rowArray.push(pieceToCreate);
         }
         data.push(rowArray);
     }
     return data;
 }
+
 function isBlackSquare(row, col) {
     return (row + col) % 2 !== 0;
 }
@@ -46,10 +55,10 @@ function renderBoard() {
     for (let r = 0; r < ROWS_NUM; r++) {
         for (let c = 0; c < COLS_NUM; c++) {
             const cell = createCellElement(r, c);
-            const pieceType = boardData[r][c];
+            const checkerData = boardData[r][c];
 
-            if (pieceType !== EMPTY_CELL) {
-                const checker = createCheckerElement(pieceType);
+            if (checkerData) {
+                const checker = createCheckerElement(checkerData);
                 cell.appendChild(checker);
             }
 
@@ -65,10 +74,10 @@ function createCellElement(row, col) {
     return cell;
 }
 
-function createCheckerElement(pieceType) {
+function createCheckerElement(checkerData) {
     const checker = document.createElement('div');
     checker.classList.add(CHECKER_CLASS);
-    checker.classList.add(pieceType === WHITE_PIECE ? WHITE_CHECKER_CLASS : BLACK_CHECKER_CLASS);
+    checker.classList.add(checkerData.color === WHITE_PIECE ? WHITE_CHECKER_CLASS : BLACK_CHECKER_CLASS);
 
     checker.addEventListener('click', (e) => {
         toggleHighlight(checker);
