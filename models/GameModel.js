793e73ecbe98c3ecfc6a6187bb1826b7421aec84
gggosle <gakhovalera2004@gmail.com>
2026-03-11 13:59:46 +0200

    Refactor board size management by consolidating row and column constants into `BOARD_SIZE` and updating related calculations and styles

--- models/GameModel.js
+++ models/GameModel.js
@@ -1,8 +1,7 @@
 import { Checker } from './Checker.js';
 
 export class GameModel {
-    static ROWS_NUM = 8;
-    static COLS_NUM = 8;
+    static BOARD_SIZE = 8;
     static PIECE_ROWS_COUNT = 3;
     static BLACK_PIECE = 2;
     static WHITE_PIECE = 1;
@@ -19,15 +18,15 @@ export class GameModel {
 
     #initializeBoardData() {
         const data = [];
-        for (let row = 0; row < GameModel.ROWS_NUM; row++) {
+        for (let row = 0; row < GameModel.BOARD_SIZE; row++) {
             const rowArray = [];
-            for (let col = 0; col < GameModel.COLS_NUM; col++) {
+            for (let col = 0; col < GameModel.BOARD_SIZE; col++) {
                 let pieceToCreate = null;
 
                 if (this.isBlackSquare(row, col)) {
                     if (row < GameModel.PIECE_ROWS_COUNT) {
                         pieceToCreate = new Checker(GameModel.WHITE_PIECE, row, col, 1);
-                    } else if (row >= GameModel.ROWS_NUM - GameModel.PIECE_ROWS_COUNT) {
+                    } else if (row >= GameModel.BOARD_SIZE - GameModel.PIECE_ROWS_COUNT) {
                         pieceToCreate = new Checker(GameModel.BLACK_PIECE, row, col, -1);
                     }
                 }
