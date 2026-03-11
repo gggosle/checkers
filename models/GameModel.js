9fc8ce0cb4f0bc6c5ecc5c40c4858fae936c5aa5
gggosle <gakhovalera2004@gmail.com>
2026-03-11 14:40:10 +0200

    Refactor move direction constants in `GameModel` by introducing `MOVE_DIR_UP` and `MOVE_DIR_DOWN`.

--- models/GameModel.js
+++ models/GameModel.js
@@ -3,6 +3,8 @@ import { Color } from './Color.js';
 
 const BOARD_SIZE = 8;
 const PIECE_ROWS_COUNT = 3;
+const MOVE_DIR_UP = 1;
+const MOVE_DIR_DOWN = -1;
 
 export class GameModel {
     #board;
@@ -24,9 +26,9 @@ export class GameModel {
 
                 if (this.isBlackSquare(row, col)) {
                     if (row < PIECE_ROWS_COUNT) {
-                        pieceToCreate = new Checker(Color.WHITE, row, col, 1);
+                        pieceToCreate = new Checker(Color.WHITE, row, col, MOVE_DIR_UP);
                     } else if (row >= BOARD_SIZE - PIECE_ROWS_COUNT) {
-                        pieceToCreate = new Checker(Color.BLACK, row, col, -1);
+                        pieceToCreate = new Checker(Color.BLACK, row, col, MOVE_DIR_DOWN);
                     }
                 }
 
